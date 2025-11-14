2b41faecc96a376c41cbd97a561c979ef9cee844
muratapk <muratapk@hotmail.com>
2025-11-14 19:57:09 +0300

    update yapıldı

---
+++ eticaretgiyim/Migrations/20251114164653_urunpaket.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eticaretgiyim.Migrations
+{
+    /// <inheritdoc />
+    public partial class urunpaket : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "urunlers",
+                columns: table => new
+                {
+                    UrunID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UrunAd = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Aciklama = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fiyat = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    StokAdet = table.Column<int>(type: "int", nullable: false),
+                    KategoriID = table.Column<int>(type: "int", nullable: false),
+                    Renk = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Beden = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GorselUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Aktif = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_urunlers", x => x.UrunID);
+                    table.ForeignKey(
+                        name: "FK_urunlers_kategorilers_KategoriID",
+                        column: x => x.KategoriID,
+                        principalTable: "kategorilers",
+                        principalColumn: "KategoriID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_urunlers_KategoriID",
+                table: "urunlers",
+                column: "KategoriID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "urunlers");
+        }
+    }
+}
