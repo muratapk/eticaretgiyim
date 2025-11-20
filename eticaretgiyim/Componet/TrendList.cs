c63ecc719a9362731777f908ad03bb69f3c41fa8
muratapk <muratapk@hotmail.com>
2025-11-20 19:45:16 +0300

    değişikler yapıldı

---
+++ eticaretgiyim/Componet/TrendList.cs
@@ -0,0 +1,19 @@
+﻿using eticaretgiyim.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eticaretgiyim.Componet
+{
+    public class TrendList:ViewComponent
+    {
+        private readonly GiyimDbContext _context;
+        public TrendList(GiyimDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var trendlist = _context.urunlers.ToList();
+            return View(trendlist);
+        }
+    }
+}
